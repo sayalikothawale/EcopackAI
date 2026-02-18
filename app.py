780b33d73d0e1efa3966652a1381fea1dbe11de2
sayali <sayalikothawale5185@gmail.com>
2026-02-18 20:28:39 +0530

    reset db structure

--- app.py
+++ app.py
@@ -198,6 +198,8 @@ def export_pdf():
 # ==============================
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+   with app.app_context():
+    db.drop_all()
+    db.create_all()
+
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
